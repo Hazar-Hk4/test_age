13b01e9927114a7edf42f14aa5993a9fc488a719
Hazar-Hk4 <hazarblbas64@gmail.com>
2024-11-23 20:25:33 +0300

    Create age.py

---
+++ age.py
@@ -0,0 +1,12 @@
+name= input('enter your name: ')
+print('hallo. '+name)
+
+age=int(input('Enter your age : '))
+if age >=75:
+    print('grait: good')
+elif age>=50:
+    print('grait; averg')
+elif age>=25:
+    print('grait; yong')
+else:
+    print('grait ; hich shtekt nanusya')
